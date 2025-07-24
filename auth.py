d80f63ccef9b9f053d194ea44718cd17e978244c
Ashutosh Dubey <ashutoshmddubey@gmail.com>
2025-07-25 02:30:13 +0530

    Create initial CLI framework

---
+++ auth.py
@@ -0,0 +1,5 @@
+def register():
+    pass
+
+def login():
+    pass
---
+++ main.py
@@ -0,0 +1,23 @@
+import auth
+
+def mainMenu():
+    while True:
+        print("\n===== Personal Finance Manager =====")
+        print("1. Register")
+        print("2. Login")
+        print("3. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            auth.register()
+        elif choice == '2':
+            auth.login()
+        elif choice == '3':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    mainMenu()
